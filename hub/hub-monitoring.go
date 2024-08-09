f21ab8d8ebdbe9ca369718ab9e35d62b8283c8ad
ridzuan5757 <ridzuan5757@gmail.com>
2024-08-09 11:44:28 +0800

    hub proto sandbox

--- hub/hub-monitoring.go
+++ hub/hub-monitoring.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	metermetric "go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 )
 
 type Response struct {
@@ -36,15 +46,81 @@ func getHub() (Response, error) {
 	return result, nil
 }
 
-func main() {
-	response, err := getHub()
+func newResource() (*resource.Resource, error) {
+	return resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("hub-proactive-monitoring"),
+			semconv.ServiceVersion("0.0.1"),
+		),
+	)
+}
+
+func newMeterProvider(res *resource.Resource) (*metric.MeterProvider, error) {
+	metricExporter, err := stdoutmetric.New()
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		return nil, err
 	}
 
-	// Print the parsed result
-	fmt.Printf("SIP: %t\n", response.SIP)
-	fmt.Printf("Capillary: %t\n", response.CAPILLARY)
-	fmt.Printf("GHL: %t\n", response.GHL)
+	meterProvider := metric.NewMeterProvider(
+		metric.WithResource(res),
+		metric.WithReader(
+			metric.NewPeriodicReader(
+				metricExporter,
+				metric.WithInterval(time.Second),
+			),
+		),
+	)
+	return meterProvider, nil
+}
+
+func main() {
+	res, err := newResource()
+	if err != nil {
+		panic(err)
+	}
+
+	meterProvider, err := newMeterProvider(res)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		err := meterProvider.Shutdown(context.Background())
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+
+	otel.SetMeterProvider(meterProvider)
+
+	var meter = otel.Meter("hub.connection")
+
+	sip, err := meter.Int64ObservableGauge(
+		"sip",
+		metermetric.WithDescription("Hub connection status with SIP"),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	meter.RegisterCallback(
+		func(_ context.Context, o metermetric.Observer) error {
+			response, err := getHub()
+			if err != nil {
+				panic(err)
+			}
+
+			var sipval int64
+			if response.SIP {
+				sipval = 1
+			} else {
+				sipval = 0
+			}
+			o.ObserveInt64(sip, sipval)
+			return nil
+		},
+		sip,
+	)
 }
