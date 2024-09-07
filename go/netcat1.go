c8cab0e635af019ddd27e379763239412dd0c56c
ridzuan5757 <ridzuan5757@gmail.com>
2024-09-07 15:07:25 +0800

    netcat client

--- go/netcat1.go
+++ go/netcat1.go
@@ -7,6 +7,13 @@ import (
 	"os"
 )
 
+// Read data from the connection and writes it to the standard output until an
+// end-of-file condition or an error occurs.
+//
+// When we are running multiple client at the same time on different terminals,
+// the second client must wait until the first client is finished because the
+// server is sequential. It only defauls with only one client at a time.
+
 func main() {
 	conn, err := net.Dial("tcp", "localhost:8000")
 	if err != nil {
